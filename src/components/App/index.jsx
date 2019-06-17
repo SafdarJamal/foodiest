345281774f59e654268057067729af9f6bd90b21
Safdar Jamal <safdar.dev@gmail.com>
2019-06-17 18:20:27 +0500

    Install material-ui icons

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,16 @@
 import React, { Component, Fragment } from 'react';
 
+import PrimaryButton from '../PrimaryButton';
+
 import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Typography variant="h2">Hello World</Typography>;
+        <Typography variant="h2">Hello World</Typography>
+        <br />
+        <PrimaryButton />
       </Fragment>
     );
   }
