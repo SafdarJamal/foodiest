a11e1b2c2bc921a5887ab5e2ee271c029f6cb79a
Safdar Jamal <safdar.dev@gmail.com>
2019-06-22 21:11:36 +0500

    add custom material theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,18 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+// Material helpers
+import { createMuiTheme } from '@material-ui/core';
 
 import palette from './palette';
+import typography from './typography';
+import overrides from './overrides';
 
 const theme = createMuiTheme({
-  palette
+  palette,
+  typography,
+  overrides,
+  zIndex: {
+    appBar: 1200,
+    drawer: 1100
+  }
 });
 
 export default theme;
