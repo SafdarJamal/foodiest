88b870da022842fc89b6b55355806bbef31d47c9
Safdar Jamal <safdar.dev@gmail.com>
2019-06-27 23:57:41 +0500

    Create SignIn Component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,13 +3,7 @@ import React, { Component } from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import theme from '../../theme';
 
-// import Typography from '@material-ui/core/Typography';
-// import Container from '@material-ui/core/Container';
-
 import Header from '../Header';
-// import CustomButton from '../CustomButton';
-// import Paper from '../Paper';
-// import SignIn from '../SignIn';
 import Landing from '../Landing';
 
 class App extends Component {
@@ -17,21 +11,6 @@ class App extends Component {
     return (
       <ThemeProvider theme={theme}>
         <Header />
-        {/* <Container style={{ marginTop: 100 }}>
-          <Paper>
-            <Typography variant="h1" style={{ marginTop: 250 }}>
-              Foodiest is the easy way to get
-              <br />
-              the food you love delivered.
-            </Typography>
-            <Typography variant="subtitle1" style={{ marginTop: 25 }}>
-              Find food you love from local restaurants and chain favorites
-            </Typography>
-
-            <br />
-            <CustomButton text="Get started" type="primary" size="large" />
-          </Paper>
-        </Container> */}
         <Landing />
       </ThemeProvider>
     );
