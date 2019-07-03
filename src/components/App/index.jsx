a967c6fc6a8aa6b87143befd4962dfc9e6a887c6
Safdar Jamal <safdar.dev@gmail.com>
2019-07-03 18:19:39 +0500

    Create Sign Up component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,7 +5,8 @@ import theme from '../../theme';
 
 import Header from '../Header';
 // import Landing from '../Landing';
-import SignIn from '../SignIn';
+// import SignIn from '../SignIn';
+import SignUp from '../SignUp';
 
 class App extends Component {
   render() {
@@ -13,7 +14,8 @@ class App extends Component {
       <ThemeProvider theme={theme}>
         <Header />
         {/* <Landing /> */}
-        <SignIn />
+        {/* <SignIn /> */}
+        <SignUp />
       </ThemeProvider>
     );
   }
---
+++ src/components/SignUp/index.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Container from '@material-ui/core/Container';
+import Paper from '../Paper';
+import Typography from '@material-ui/core/Typography';
+import InputField from '../InputField';
+import CustomButton from '../CustomButton';
+
+class SignIn extends Component {
+  render() {
+    return (
+      <Container style={{ marginTop: 100, width: 600 }}>
+        <Paper class0="root">
+          <Typography variant="h1" align="center">
+            Sign Up
+          </Typography>
+          <InputField label="Email" type="email" />
+          <InputField label="Passowrd" type="password" InputProps={true} />
+          <div style={{ textAlign: 'right' }}>
+            <CustomButton type="primary" text="Next" size="large" />
+          </div>
+        </Paper>
+      </Container>
+    );
+  }
+}
+
+export default SignIn;
