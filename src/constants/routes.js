527c2c4566ad558355ac3ada9e5f38504d36df47
Safdar Jamal <safdar.dev@gmail.com>
2019-07-07 23:50:34 +0500

    Create router constants

---
+++ src/constants/routes.js
@@ -0,0 +1,8 @@
+const LANDING = '/';
+const SIGNUP = '/signup';
+const VERIFICATION = '/verification';
+const SIGNIN = '/signin';
+const HOME = '/home';
+const RESET_PASSWORD = '/reset-password';
+
+export { LANDING, SIGNUP, VERIFICATION, SIGNIN, HOME, RESET_PASSWORD };
