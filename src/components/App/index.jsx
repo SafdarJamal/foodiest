c40a1aa3af51c0c1da84a45aed5e49a144eaef3f
Safdar Jamal <safdar.dev@gmail.com>
2019-07-08 00:57:16 +0500

    Implement React Router

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,30 +1,34 @@
 import React, { Component } from 'react';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+
 import { ThemeProvider } from '@material-ui/styles';
 import theme from '../../theme';
 
 import Header from '../Header';
-import Landing from '../Landing';
-// import AccountType from '../AccountType';
-// import SignUp from '../SignUp';
-// import SignIn from '../SignIn';
-// import EmailVerification from '../EmailVerification';
-// import PasswordReset from '../PasswordReset';
-// import Home from '../Home';
+import LandingPage from '../Landing';
+import AccountType from '../AccountType';
+import SignUp from '../SignUp';
+import EmailVerification from '../EmailVerification';
+import SignIn from '../SignIn';
+import PasswordReset from '../PasswordReset';
+import Home from '../Home';
 
 class App extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <Header isLanding={true} />
-        <Landing />
-        {/* <AccountType /> */}
-        {/* <SignUp /> */}
-        {/* <SignIn /> */}
-        {/* <EmailVerification /> */}
-        {/* <PasswordReset /> */}
-        {/* <Home /> */}
-      </ThemeProvider>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <Header isLanding={true} />
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.SIGNUP} component={SignUp} />
+          <Route path={ROUTES.VERIFICATION} component={EmailVerification} />
+          <Route path={ROUTES.SIGNIN} component={SignIn} />
+          <Route path={ROUTES.RESET_PASSWORD} component={PasswordReset} />
+          <Route path={ROUTES.HOME} component={Home} />
+        </ThemeProvider>
+      </Router>
     );
   }
 }
