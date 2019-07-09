7e1e5f27113bd6a76485596678bd34f6be30b62c
Safdar Jamal <safdar.dev@gmail.com>
2019-07-09 17:15:05 +0500

    Move firebase to services directory

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import store from './store';
 
-import Firebase, { FirebaseContext } from './firebase';
+import Firebase, { FirebaseContext } from './services/firebase';
 
 ReactDOM.render(
   <Provider store={store}>
--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -1,19 +1,12 @@
 import app from 'firebase/app';
-import firebaseConfig from '../config/firebaseConfig';
+import firebaseConfig from '../../config/firebaseConfig';
 
 // const firebase = app.initializeApp(firebaseConfig);
-
 // export default firebase;
 
 class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
-
-    this.doThis = this.doThis.bind(this);
-  }
-
-  doThis() {
-    return 'doingThis';
   }
 }
 
