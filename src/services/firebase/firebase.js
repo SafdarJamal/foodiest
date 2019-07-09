b9ba2286208e1f0fefa6b6b3040cbd1978a190d5
Safdar Jamal <safdar.dev@gmail.com>
2019-07-09 17:38:48 +0500

    Create firebase auth methods

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 import firebaseConfig from '../../config/firebaseConfig';
 
 // const firebase = app.initializeApp(firebaseConfig);
@@ -7,7 +8,24 @@ import firebaseConfig from '../../config/firebaseConfig';
 class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
+
+    this.auth = app.auth();
   }
+
+  // Auth API
+  signUp = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  verify = () => this.auth.currentUser.sendEmailVerification();
+
+  signIn = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  passwordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  passwordUpdate = password => this.auth.currentUser.updatePassword(password);
+
+  signOut = () => this.auth.signOut();
 }
 
 export default Firebase;
