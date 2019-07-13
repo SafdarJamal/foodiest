8fba4e79065e0caf1e93201968ad05f563ac6330
Safdar Jamal <safdar.dev@gmail.com>
2019-07-13 16:23:47 +0500

    Update authReducer with payload

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,15 +1,15 @@
 import { SIGN_IN, SIGN_OUT } from '../constants/actionTypes';
 
 const INITIAL_STATE = {
-  isSignedIn: null
+  user: null
 };
 
 const authReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SIGN_IN:
-      return { ...state, isSignedIn: true };
+      return { ...state, user: action.payload };
     case SIGN_OUT:
-      return { ...state, isSignedIn: false };
+      return { ...state, user: null };
     default:
       return state;
   }
