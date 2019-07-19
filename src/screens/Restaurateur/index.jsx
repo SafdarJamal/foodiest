08a0de330bd02e84cdae5af1d43752128aacf8e0
Safdar Jamal <safdar.dev@gmail.com>
2019-07-19 22:59:31 +0500

    Check nested routing

--- src/screens/Restaurateur/index.jsx
+++ src/screens/Restaurateur/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 
 import Dashboard from './Dashboard';
 
 export default () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
+    </Switch>
   );
 };
