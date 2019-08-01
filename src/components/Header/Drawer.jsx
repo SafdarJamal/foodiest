d792f39c3328a6dfdb11c5fd2b543a36543135ad
Safdar Jamal <safdar.dev@gmail.com>
2019-08-01 22:12:29 +0500

    Create Drawer Header

--- src/components/Header/Drawer.jsx
+++ src/components/Header/Drawer.jsx
@@ -7,8 +7,13 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import HomeIcon from '@material-ui/icons/Home';
+import SignInIcon from '@material-ui/icons/AccountCircle';
+import SignUpIcon from '@material-ui/icons/PersonAdd';
+import Typography from '@material-ui/core/Typography';
+import logo from '../../assets/images/logo.png';
+
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles({
   list: {
@@ -36,6 +41,15 @@ export default () => {
     setState({ ...state, [side]: open });
   };
 
+  const [selectedIndex, setSelectedIndex] = React.useState(null);
+
+  const handleListItemClick = (event, index) => {
+    setSelectedIndex(index);
+  };
+
+  const iconList = [<HomeIcon />, <SignInIcon />, <SignUpIcon />];
+  const linkList = ['/', '/signin', '/signup/type'];
+
   const sideList = side => (
     <div
       className={classes.list}
@@ -43,25 +57,28 @@ export default () => {
       onClick={toggleDrawer(side, false)}
       onKeyDown={toggleDrawer(side, false)}
     >
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
+      <div style={{ backgroundColor: '#eaeff1', padding: 20 }}>
+        <Link to="/" style={{ textDecoration: 'none', cursor: 'pointer' }}>
+          <img src={logo} alt="Foodiest" height="58" title="We Love Foodies" />
+        </Link>
+      </div>
+
       <Divider />
+
       <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
+        {['Home', 'Sign In', 'Sign Up'].map((text, index) => (
+          <Link to={linkList[index]} style={{ textDecoration: 'none' }}>
+            <ListItem
+              button
+              key={text}
+              selected={selectedIndex === index}
+              onClick={event => handleListItemClick(event, index)}
+            >
+              <ListItemIcon>{iconList[index]}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+            {index !== linkList.length - 1 && <Divider />}
+          </Link>
         ))}
       </List>
     </div>
