154c456ba8ae3f7bb01b5675c6dd0c7d0d46f1ae
Safdar Jamal <safdar.dev@gmail.com>
2019-08-03 21:38:09 +0500

    Refactor nested routes

--- src/routes/private/Foodie.jsx
+++ src/routes/private/Foodie.jsx
@@ -1,17 +1,23 @@
 import React, { lazy } from 'react';
 import { connect } from 'react-redux';
-import { Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import * as USER_TYPES from '../../constants/userTypes';
 import pMinDelay from 'p-min-delay';
 
-const Foodie = lazy(() => pMinDelay(import('../../screens/Foodie'), 2000));
+const Home = lazy(() => pMinDelay(import('../../screens/Foodie/Home'), 2000));
+const NotFound = lazy(() => import('../../components/NotFound'));
 
 const FoodieRoute = ({ user }) => {
   if (user) {
     if (user.isVerified) {
       if (user.type === USER_TYPES.FOODIE) {
-        return <Route component={Foodie} />;
+        return (
+          <Switch>
+            <Route exact path={ROUTES.HOME} component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        );
       } else if (user.type === USER_TYPES.RESTAURATEUR) {
         return <Redirect to={ROUTES.DASHBOARD} />;
       }
--- src/routes/private/Restaurateur.jsx
+++ src/routes/private/Restaurateur.jsx
@@ -1,19 +1,25 @@
 import React, { lazy } from 'react';
 import { connect } from 'react-redux';
-import { Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import * as USER_TYPES from '../../constants/userTypes';
 import pMinDelay from 'p-min-delay';
 
-const Restaurateur = lazy(() =>
-  pMinDelay(import('../../screens/Restaurateur'), 2000)
+const Dashboard = lazy(() =>
+  pMinDelay(import('../../screens/Restaurateur/Dashboard'), 2000)
 );
+const NotFound = lazy(() => import('../../components/NotFound'));
 
 const RestaurateurRoute = ({ user }) => {
   if (user) {
     if (user.isVerified) {
       if (user.type === USER_TYPES.RESTAURATEUR) {
-        return <Route component={Restaurateur} />;
+        return (
+          <Switch>
+            <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
+            <Route component={NotFound} />
+          </Switch>
+        );
       } else if (user.type === USER_TYPES.FOODIE) {
         return <Redirect to={ROUTES.HOME} />;
       }
