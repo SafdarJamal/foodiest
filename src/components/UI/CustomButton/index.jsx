3b8b9986fc051a37ed2b5a8ce78d4eca6a3d0920
Safdar Jamal <safdar.dev@gmail.com>
2019-09-24 23:21:18 +0500

    Implement PropTypes in CustomButton, and Make it Flexible

--- src/components/UI/CustomButton/index.jsx
+++ src/components/UI/CustomButton/index.jsx
@@ -1,21 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 
 const CustomButton = props => {
-  const { variant, type, text, size, onClick, disabled, disableRipple } = props;
+  const {
+    children,
+    variant,
+    type,
+    size,
+    onClick,
+    disabled,
+    disableRipple
+  } = props;
 
   return (
     <Button
       variant={variant}
-      color={type === 'primary' ? type : 'default'}
+      color={type}
       size={size}
       onClick={onClick}
       disabled={disabled}
       disableRipple={disableRipple}
     >
-      {text || type || 'Default'}
+      {children}
     </Button>
   );
+};
+
+CustomButton.propTypes = {
+  children: PropTypes.string.isRequired,
+  variant: PropTypes.string,
+  type: PropTypes.string,
+  size: PropTypes.string,
+  onClick: PropTypes.func,
+  disabled: PropTypes.bool,
+  disableRipple: PropTypes.bool
+};
+
+CustomButton.defaultProps = {
+  variant: 'text',
+  type: 'default',
+  size: 'small',
+  onClick: null,
+  disabled: false,
+  disableRipple: false
 };
 
 export default CustomButton;
