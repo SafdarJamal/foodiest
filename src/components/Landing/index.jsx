202554e9b82cfcfd20ecdbd7b5e032c428e4448d
Safdar Jamal <safdar.dev@gmail.com>
2019-10-04 20:42:52 +0500

    Replace inline css with css modules, in Landing component

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './style.module.css';
 import { Link } from 'react-router-dom';
 
 import * as ROUTES from '../../constants/routes';
@@ -8,30 +9,30 @@ import Paper from '../UI/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import CustomButton from '../UI/CustomButton';
-import heroImage from '../../assets/images/eats.jpg';
+import heroImg from '../../assets/images/eats.jpg';
 
 const Landing = () => {
   return (
-    <Container style={{ marginTop: 125 }}>
+    <Container className={styles.container}>
       <Paper>
         <Grid container>
-          <Grid item xs={4} style={{ marginLeft: 25 }}>
-            <Typography variant="h1" style={{ marginTop: 150 }}>
+          <Grid item xs={4} className={styles.titleWrapper}>
+            <Typography variant="h1" className={styles.title}>
               Foodiest is the easy way to get the food you love delivered.
             </Typography>
-            <Typography variant="subtitle1" style={{ marginTop: 50 }}>
+            <Typography variant="subtitle1" className={styles.subTitle}>
               Find food you love from local restaurants and chain favorites
             </Typography>
 
             <br />
-            <Link to={ROUTES.SIGNUP} style={{ textDecoration: 'none' }}>
+            <Link to={ROUTES.SIGNUP} className={styles.link}>
               <CustomButton variant="contained" type="primary" size="large">
                 Get Started
               </CustomButton>
             </Link>
           </Grid>
           <Grid item xs={7}>
-            <img src={heroImage} alt="BurgerImage" height="650" width="111%" />
+            <img src={heroImg} alt="BurgerImage" className={styles.heroImg} />
           </Grid>
         </Grid>
       </Paper>
