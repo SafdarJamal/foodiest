d9b4e7901e58c7799eda297f9ec032a985722b14
Safdar Jamal <safdar.dev@gmail.com>
2019-10-06 21:48:32 +0500

    Test SearchBar component

--- src/components/Foodie/Home/index.jsx
+++ src/components/Foodie/Home/index.jsx
@@ -5,8 +5,18 @@ import Container from '@material-ui/core/Container';
 import Paper from '../../UI/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+// import InputField from '../../UI/InputField';
+import SearchBar from '../../UI/SearchBar';
 
 const Home = () => {
+  const setSearchInput = value => {
+    console.log(value);
+  };
+
+  const search = value => {
+    console.log(value);
+  };
+
   return (
     <Container className={styles.container}>
       <Paper className="root">
@@ -15,6 +25,8 @@ const Home = () => {
             <Typography variant="h1" className={styles.title}>
               Home
             </Typography>
+            {/* <InputField focus={false} label="What are you craving?" /> */}
+            <SearchBar onChange={setSearchInput} onRequestSearch={search} />
           </Grid>
         </Grid>
       </Paper>
