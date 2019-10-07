cc27b1613ad8c6dd071d239dd8522dc6a54d96da
Safdar Jamal <safdar.dev@gmail.com>
2019-10-07 21:34:26 +0500

    Make Home class component

--- src/components/Foodie/Home/index.jsx
+++ src/components/Foodie/Home/index.jsx
@@ -1,37 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './style.module.css';
 
 import Container from '@material-ui/core/Container';
 import Paper from '../../UI/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-// import InputField from '../../UI/InputField';
 import SearchBar from '../../UI/SearchBar';
 
-const Home = () => {
-  const setSearchInput = value => {
-    console.log(value);
-  };
+class Home extends Component {
+  constructor(props) {
+    super(props);
 
-  const search = value => {
-    console.log(value);
-  };
+    this.state = {
+      searchValue: ''
+    };
 
-  return (
-    <Container className={styles.container}>
-      <Paper className="root">
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <Typography variant="h1" className={styles.title}>
-              Home
-            </Typography>
-            {/* <InputField focus={false} label="What are you craving?" /> */}
-            <SearchBar onChange={setSearchInput} onRequestSearch={search} />
+    this.setSearchValue = this.setSearchValue.bind(this);
+  }
+
+  setSearchValue(searchValue) {
+    console.log(searchValue);
+    this.setState({ searchValue });
+  }
+
+  search(searchValue) {
+    console.log(searchValue);
+  }
+
+  render() {
+    return (
+      <Container className={styles.container}>
+        <Paper className="root">
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography variant="h1" className={styles.title}>
+                Home
+              </Typography>
+              <SearchBar
+                placeholder="What are you craving?"
+                value={this.state.searchValue}
+                onChange={this.setSearchValue}
+                onRequestSearch={this.search}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-      </Paper>
-    </Container>
-  );
-};
+        </Paper>
+      </Container>
+    );
+  }
+}
 
 export default Home;
