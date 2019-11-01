7c27e4155c4185212016fec09df157d91a651950
Safdar Jamal <safdar.dev@gmail.com>
2019-11-01 22:20:23 +0500

    Create cards in Home component

--- src/components/Foodie/Home/index.jsx
+++ src/components/Foodie/Home/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styles from './style.module.css';
 
 import Container from '@material-ui/core/Container';
@@ -6,27 +6,87 @@ import Paper from '../../UI/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import SearchBar from '../../UI/SearchBar';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import foodie from '../../../assets/images/foodie.jpg';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  card: {
+    margin: theme.spacing(2)
+  }
+}));
 
 const Home = props => {
+  const classes = useStyles();
+
   return (
-    <Container className={styles.container}>
-      <Paper className="root">
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <Typography variant="h1" className={styles.title}>
-              Home
-            </Typography>
-            <SearchBar
-              placeholder="What are you craving?"
-              value={props.value}
-              onChange={props.onChange}
-              onRequestSearch={props.onRequestSearch}
-              onCancelSearch={props.onCancelSearch}
-            />
+    <Fragment>
+      <Container className={styles.container}>
+        <Paper className="root">
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography variant="h1" className={styles.title}>
+                Home
+              </Typography>
+              <SearchBar
+                placeholder="What are you craving?"
+                value={props.value}
+                onChange={props.onChange}
+                onRequestSearch={props.onRequestSearch}
+                onCancelSearch={props.onCancelSearch}
+              />
+            </Grid>
           </Grid>
+        </Paper>
+      </Container>
+
+      <br />
+
+      <Container>
+        <Grid container>
+          {[
+            'LA Breakfast Club',
+            `McDonald's®`,
+            'Turo Cafe & Grill',
+            'Jamba Juice',
+            'The Halal Guys',
+            'Starbucks®'
+          ].map((item) => (
+            <Grid item xs={4}>
+              <Card className={classes.card}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={foodie}
+                    className={styles.cardImg}
+                    alt="Foodie"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {item}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      A foodie is a person who has an ardent or refined interest
+                      in food.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
         </Grid>
-      </Paper>
-    </Container>
+      </Container>
+    </Fragment>
   );
 };
 
