df74d106d0038e375f5f05b9c10059d9952cfa76
Safdar Jamal <safdar.dev@gmail.com>
2020-01-26 19:12:49 +0500

    Add Head in signup/foodie screen

--- src/screens/signup/foodie/index.js
+++ src/screens/signup/foodie/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Head from '../../../components/Head';
 import SignUpContainer from '../../../containers/SignUpContainer';
 
 const SignUpFoodieScreen = () => {
-  return <SignUpContainer />;
+  return (
+    <>
+      <Head title="Sign Up Foodie" />
+      <SignUpContainer />;
+    </>
+  );
 };
 
 export default SignUpFoodieScreen;
