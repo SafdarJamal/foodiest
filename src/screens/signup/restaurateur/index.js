12f402d02d8c04f793d30c4248aa22f89587542a
Safdar Jamal <safdar.dev@gmail.com>
2020-01-26 19:12:31 +0500

    Add Head in 404 signup/restaurateur screen

--- src/screens/signup/restaurateur/index.js
+++ src/screens/signup/restaurateur/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Head from '../../../components/Head';
 import SignUpContainer from '../../../containers/SignUpContainer';
 
 const SignUpRestaurateurScreen = () => {
-  return <SignUpContainer />;
+  return (
+    <>
+      <Head title="Sign Up Restaurateur" />
+      <SignUpContainer />;
+    </>
+  );
 };
 
 export default SignUpRestaurateurScreen;
